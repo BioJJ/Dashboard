97f99ed337398418c8916572b85d7c99eda024ae
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2021-02-24 08:09:53 -0400

    starting the project, screens and initial components

---
+++ src/components/ModalCreateAccount/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex justify-between" id="modal-login">
+    <h1 class="text-4xl font-black text-gray-800">
+      Entre na sua conta
+    </h1>
+
+    <button
+      @click="close"
+      class="text-4xl text-gray-600 focus:outline-none"
+    >
+      &times;
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
